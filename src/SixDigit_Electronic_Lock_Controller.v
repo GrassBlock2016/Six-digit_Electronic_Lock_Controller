607870836944fad36e9c71f1367c77c85e84a1af
jesse <12345@678.com>
2024-06-04 23:27:45 +0800

    实例化

--- src/SixDigit_Electronic_Lock_Controller.v
+++ src/SixDigit_Electronic_Lock_Controller.v
@@ -4,5 +4,5 @@ module SixDigit_Electronic_Lock_Controller(s,a1,a2,a3,a4,a5,a6,b1,b2,b3,b4,b5,b6
 	input [3:0] a1, a2, a3, a4, a5, a6, b1, b2, b3, b4, b5, b6;
 	output reg c;
 	
-	judge(s,a1,a2,a3,a4,a5,a6,b1,b2,b3,b4,b5,b6,c);
+	judge judge_1(s,a1,a2,a3,a4,a5,a6,b1,b2,b3,b4,b5,b6,c);
 endmodule
