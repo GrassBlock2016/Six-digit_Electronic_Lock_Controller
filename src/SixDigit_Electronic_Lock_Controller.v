be9107f184905e2bca8daeead808bc7399bb5078
GrassBlock2016 <598926542@qq.com>
2024-06-05 15:14:08 +0800

    尝试在主模块中组合了寄存器和比较器

--- src/SixDigit_Electronic_Lock_Controller.v
+++ src/SixDigit_Electronic_Lock_Controller.v
@@ -1,8 +1,60 @@
-module SixDigit_Electronic_Lock_Controller(s,a1,a2,a3,a4,a5,a6,b1,b2,b3,b4,b5,b6,c);
-	
-	input s;
-	input [3:0] a1, a2, a3, a4, a5, a6, b1, b2, b3, b4, b5, b6;
-	output reg c;
-	
-	judge judge_1(s,a1,a2,a3,a4,a5,a6,b1,b2,b3,b4,b5,b6,c);
+module SixDigit_Electronic_Lock_Controller(
+    input j,              // judge，开启判断
+    input m,              // mode，切换模式，0为设置密码，1为输入密码
+    input [3:0] in1, in2, in3, in4, in5, in6, // 输入
+    input clk, clr,       // 时钟信号
+    output [3:0] out1, out2, out3, out4, out5, out6, // 输出
+    output reg res        // 比较结果
+);
+
+    // 内部信号
+    wire [3:0] passwd_out1, passwd_out2, passwd_out3, passwd_out4, passwd_out5, passwd_out6;
+    reg [3:0] out1_reg, out2_reg, out3_reg, out4_reg, out5_reg, out6_reg;
+    reg judge_enable;
+
+    // 输出赋值
+    assign out1 = out1_reg;
+    assign out2 = out2_reg;
+    assign out3 = out3_reg;
+    assign out4 = out4_reg;
+    assign out5 = out5_reg;
+    assign out6 = out6_reg;
+
+    // 密码寄存器实例化
+    passwd_register password(in1,in2,in3,in4,in5,in6,passwd_out1, passwd_out2, passwd_out3, passwd_out4, passwd_out5, passwd_out6,clr,clk);
+
+    // 比较模块实例化
+    judge judge_1(j,in1,in2,in3,in4,in5,in6,passwd_out1, passwd_out2, passwd_out3, passwd_out4, passwd_out5, passwd_out6,res);
+
+    // 模式选择和输出赋值的控制逻辑
+    always @(posedge clk or posedge clr) begin
+        if (clr) begin
+            out1_reg <= 4'b0000;
+            out2_reg <= 4'b0000;
+            out3_reg <= 4'b0000;
+            out4_reg <= 4'b0000;
+            out5_reg <= 4'b0000;
+            out6_reg <= 4'b0000;
+        end else begin
+            if (m == 0) begin
+                // 模式0：设置密码
+                out1_reg <= in1;
+                out2_reg <= in2;
+                out3_reg <= in3;
+                out4_reg <= in4;
+                out5_reg <= in5;
+                out6_reg <= in6;
+            end else begin
+                // 模式1：输入密码并进行判断
+                judge_enable <= j;
+                out1_reg <= passwd_out1;
+                out2_reg <= passwd_out2;
+                out3_reg <= passwd_out3;
+                out4_reg <= passwd_out4;
+                out5_reg <= passwd_out5;
+                out6_reg <= passwd_out6;
+            end
+        end
+    end
+
 endmodule
--- src/passwd_register.v
+++ src/passwd_register.v
@@ -1,14 +1,15 @@
 // 24Î»ÃÜÂë¼Ä´æÆ÷×é
-module passwd_register(d,q,clr,clk);
+module passwd_register(d1,d2,d3,d4,d5,d6,q1,q2,q3,q4,q5,q6,clr,clk);
 	
 	input clk,clr;
-	input [23:0] d;
-	output [23:0] q;
+	input [3:0] d1,d2,d3,d4,d5,d6;
+	output [3:0] q1,q2,q3,q4,q5,q6;
 	
-	register r1(d[3:0],q[3:0],clk,clr);
-	register r2(d[7:4],q[7:4],clk,clr);
-	register r3(d[11:8],q[11:8],clk,clr);
-	register r4(d[15:12],q[15:12],clk,clr);
-	register r5(d[19:16],q[19:16],clk,clr);
-	register r6(d[23:20],q[23:20],clk,clr);
+	register r1(d1[3:0],q1[3:0],clk,clr);
+	register r2(d2[3:0],q2[3:0],clk,clr);
+	register r3(d3[3:0],q3[3:0],clk,clr);
+	register r4(d4[3:0],q4[3:0],clk,clr);
+	register r5(d5[3:0],q5[3:0],clk,clr);
+	register r6(d6[3:0],q6[3:0],clk,clr);
+	
 endmodule
