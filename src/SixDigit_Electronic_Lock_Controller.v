ca8ec6671f6f5869b639aab4dc17d810b13dc255
GrassBlock2016 <598926542@qq.com>
2024-06-17 23:54:54 +0800

    更新：周一实验后

--- src/SixDigit_Electronic_Lock_Controller.v
+++ src/SixDigit_Electronic_Lock_Controller.v
@@ -6,12 +6,13 @@ module SixDigit_Electronic_Lock_Controller(
     output res,        // 比较结果
     input a0, a1,            // 高、中、低位输入与判断选择
 	output led,       // LED输出
-	input ps0,ps1,     // 多密码选择
-	output [3:0] s11,s12,s13,s14,s15,s16,    // debug用，先勿删
-	output [3:0] s21,s22,s23,s24,s25,s26,    // debug用，先勿删
-	output [3:0] s31,s32,s33,s34,s35,s36,    // debug用，先勿删
-	output [3:0] s41,s42,s43,s44,s45,s46,    // debug用，先勿删
-	output [3:0] c1,c2,c3,c4,c5,c6    // debug用，先勿删
+	input ps0,ps1,    // 多密码选择
+	output [1:0] eo
+	//output [3:0] s11,s12,s13,s14,s15,s16,    // debug用，先勿删
+	//output [3:0] s21,s22,s23,s24,s25,s26,    // debug用，先勿删
+	//output [3:0] s31,s32,s33,s34,s35,s36,    // debug用，先勿删
+	//output [3:0] s41,s42,s43,s44,s45,s46,    // debug用，先勿删
+	//output [3:0] c1,c2,c3,c4,c5,c6    // debug用，先勿删
 );
 
     // 内部信号
@@ -23,15 +24,17 @@ module SixDigit_Electronic_Lock_Controller(
     wire [3:0] in1, in2, in3, in4, in5, in6, checked_inA, checked_inB;
     wire y0, y1, y2, y3;      // 高位输入、中位输入、低位输入、判断选择
     wire p0, p1, p2, p3;	  // 密码1、密码2、密码3、密码4
-	wire res1,res2,res3,res4; // 每个密码的比较结果
+	wire res1,res2,res3,res4; // 每个密码的比较结果  555555555555555555
 	wire [1:0] error_count;
 	wire tick;
 	wire error_flag;		// 切换模式报错信号
     reg [3:0] out1_reg, out2_reg, out3_reg, out4_reg, out5_reg, out6_reg;
 	reg start_flashing;
-	reg res_latched;  // 锁存的比较结果
+	reg res_reg;
+	wire res_tmp;
+	//reg res_latched;  // 锁存的比较结果
 	
-	assign res = (res1|res2|res3|res4) & m;
+	assign res_tmp = (res1|res2|res3|res4) & m;
 	
 	// 如果输入非进制数则显示E(error)
 	assign checked_inA = (inA > 4'b1001) ? 4'b1110 : inA;
@@ -44,38 +47,40 @@ module SixDigit_Electronic_Lock_Controller(
     assign out4 = out4_reg;
     assign out5 = out5_reg;
     assign out6 = out6_reg;
+	assign res = res_reg;
+	assign eo = error_count;
 
 	// 以下为debug用，先勿删
-	assign s11=set1_out1;
-	assign s12=set1_out2;
-	assign s13=set1_out3;
-	assign s14=set1_out4;
-	assign s15=set1_out5;
-	assign s16=set1_out6;
-	assign s21=set2_out1;
-	assign s22=set2_out2;
-	assign s23=set2_out3;
-	assign s24=set2_out4;
-	assign s25=set2_out5;
-	assign s26=set2_out6;
-	assign s31=set3_out1;
-	assign s32=set3_out2;
-	assign s33=set3_out3;
-	assign s34=set3_out4;
-	assign s35=set3_out5;
-	assign s36=set3_out6;
-	assign s41=set4_out1;
-	assign s42=set4_out2;
-	assign s43=set4_out3;
-	assign s44=set4_out4;
-	assign s45=set4_out5;
-	assign s46=set4_out6;
-	assign c1=cin_out1;
-	assign c2=cin_out2;
-	assign c3=cin_out3;
-	assign c4=cin_out4;
-	assign c5=cin_out5;
-	assign c6=cin_out6;
+	//assign s11=set1_out1;
+	//assign s12=set1_out2;
+	//assign s13=set1_out3;
+	//assign s14=set1_out4;
+	//assign s15=set1_out5;
+	//assign s16=set1_out6;
+	//assign s21=set2_out1;
+	//assign s22=set2_out2;
+	//assign s23=set2_out3;
+	//assign s24=set2_out4;
+	//assign s25=set2_out5;
+	//assign s26=set2_out6;
+	//assign s31=set3_out1;
+	//assign s32=set3_out2;
+	//assign s33=set3_out3;
+	//assign s34=set3_out4;
+	//assign s35=set3_out5;
+	//assign s36=set3_out6;
+	//assign s41=set4_out1;
+	//assign s42=set4_out2;
+	//assign s43=set4_out3;
+	//assign s44=set4_out4;
+	//assign s45=set4_out5;
+	//assign s46=set4_out6;
+	//assign c1=cin_out1;
+	//assign c2=cin_out2;
+	//assign c3=cin_out3;
+	//assign c4=cin_out4;
+	//assign c5=cin_out5;
+	//assign c6=cin_out6;
 
     // 模式选择24译码器实例化
     yima2to4 yima_mode(a0, a1, 0, y0, y1, y2, y3);
@@ -117,13 +122,15 @@ module SixDigit_Electronic_Lock_Controller(
     );
 
     // 比较模块实例化
-    judge jg1(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set1_out1, set1_out2, set1_out3, set1_out4, set1_out5, set1_out6, res1);
-	judge jg2(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set2_out1, set2_out2, set2_out3, set2_out4, set2_out5, set2_out6, res2);
-	judge jg3(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set3_out1, set3_out2, set3_out3, set3_out4, set3_out5, set3_out6, res3);
-	judge jg4(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set4_out1, set4_out2, set4_out3, set4_out4, set4_out5, set4_out6, res4);
+	reg rd1,rd2,rd3,rd4;
+    judge jg1(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set1_out1, set1_out2, set1_out3, set1_out4, set1_out5, set1_out6, res1,rd1);
+	judge jg2(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set2_out1, set2_out2, set2_out3, set2_out4, set2_out5, set2_out6, res2,rd2);
+	judge jg3(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set3_out1, set3_out2, set3_out3, set3_out4, set3_out5, set3_out6, res3,rd3);
+	judge jg4(y3, cin_out1, cin_out2, cin_out3, cin_out4, cin_out5, cin_out6, set4_out1, set4_out2, set4_out3, set4_out4, set4_out5, set4_out6, res4,rd4);
+	wire rd = rd1|rd2|rd3|rd4;
 	
 	// 错误计数器模块实例化
-	error_counter error_cnt(y3, res, error_count);
+	error_counter error_cnt(y3,clk, res_tmp, error_count);
 	
 	// 定时器模块实例化
 	timer tm(true_clk, tick);
@@ -132,30 +139,33 @@ module SixDigit_Electronic_Lock_Controller(
 	led_flasher led_fls(tick, start_flashing, led);
 	
 	// 模式切换检查模块实例化
-	mode_switch_checker mode_chk(m, res_latched, error_flag);
+	//mode_switch_checker mode_chk(m, res_latched, error_flag);
 	
     // 模式选择和输出赋值的控制逻辑
     always @(posedge clk) begin
-		if (y3) begin
-			res_latched <= res;
-		end else begin
-			res_latched = 1'b0;
-		end
+		//if (y3) begin
+		//	res_latched <= res;
+		//end else begin
+		//	res_latched = 1'b0;
+		//end
 		
-		if (error_flag) begin
-			out1_reg <= 4'b1110;
-			out2_reg <= 4'b1110;
-			out3_reg <= 4'b1110;
-			out4_reg <= 4'b1110;
-			out5_reg <= 4'b1110;
-			out6_reg <= 4'b1110;
-		end else begin
+		//if (error_flag) begin
+		//	out1_reg <= 4'b1110;
+		//	out2_reg <= 4'b1110;
+		//	out3_reg <= 4'b1110;
+		//	out4_reg <= 4'b1110;
+		//	out5_reg <= 4'b1110;
+		//	out6_reg <= 4'b1110;
+		//end else begin
         	case ({y2, y1, y0})
             	3'b001: {out1_reg, out2_reg} <= {checked_inA, checked_inB};
             	3'b010: {out3_reg, out4_reg} <= {checked_inA, checked_inB};
             	3'b100: {out5_reg, out6_reg} <= {checked_inA, checked_inB};
         	endcase
-		end 
+		//end 
+		if (y3) begin
+			res_reg <= res_tmp;
+		end
 
 		if (!m & clr) begin
 			out1_reg <= 4'b0000;
--- src/error_counter.v
+++ src/error_counter.v
@@ -1,14 +1,23 @@
 // 错误计数器，记录错误次数
 module error_counter(
 	input j,	// 传入判断
+	input rd,
     input correct,
     output reg [1:0] error_count
 );
-    always @(posedge j) begin	// 当j下降沿开始计数
-        if (correct) begin
-            error_count <= 2'b00;
-        end else if (error_count < 2'b11) begin
-            error_count <= error_count + 1;
-        end
-    end
+    always @(negedge rd) begin	// 当j上升沿开始计数
+		if(j) begin
+        	if (correct) begin
+            	error_count <= 2'b00;
+        	end else if (error_count < 2'b11) begin
+            	if (error_count == 2'b00) begin
+					error_count <= 2'b01;
+				end else if (error_count == 2'b01) begin
+					error_count <= 2'b10;
+				end else if (error_count == 2'b10) begin
+					error_count <= 2'b11;
+				end
+        	end
+    	end 
+	end
 endmodule
