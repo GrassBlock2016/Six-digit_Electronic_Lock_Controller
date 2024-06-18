6e85fed6228ef0dc38c14d12a9d0810c88e52125
GrassBlock2016 <598926542@qq.com>
2024-06-18 18:37:02 +0800

    部分优化

--- src/error_counter.v
+++ src/error_counter.v
@@ -9,17 +9,15 @@ module error_counter(
 		if (j) begin
         	if (correct) begin
            		error_count <= 2'b00;
-	       	end else if (error_count < 2'b11) begin
+	       	end else if (error_count <= 2'b11) begin
     	    	if (error_count == 2'b00) begin
 					error_count <= 2'b01;
 				end else if (error_count == 2'b01) begin
 					error_count <= 2'b10;
 				end else if (error_count == 2'b10) begin
 					error_count <= 2'b11;
-				end
-			//	error_count <= error_count + 1;
-        	end else begin
-				error_count <= 2'b00;
+				end else if (error_count == 2'b11) begin
+					error_count <= 2'b01;
 			end
 		end
 	end
